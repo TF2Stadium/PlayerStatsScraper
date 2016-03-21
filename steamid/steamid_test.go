1aeed273913547fda95e7247d33e4fc41a8188af
Vibhav Pant <vibhavp@gmail.com>
2016-03-21 10:15:11 +0530

    Use Error and NoError, add test for SteamIDsEqual

--- steamid/steamid_test.go
+++ steamid/steamid_test.go
@@ -12,37 +12,46 @@ func TestCommunityIdToSteamId(t *testing.T) {
 	assert.Equal(t, res, "[U:1:38808257:1]")
 
 	res, err = CommIdToSteamId("99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999")
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 
 	res, err = CommIdToLegacySteamId("76561197999073985")
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, res, "STEAM_0:1:19404128")
 
 	_, err = CommIdToLegacySteamId("999999999999999999999999999999999999999999999999999")
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestNewSteamIdToCommunityId(t *testing.T) {
 	res, err := SteamIdToCommId("[U:1:38808257]")
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, res, "76561197999073985")
 
 	res, err = SteamIdToCommId("U:1:38808257:1")
-	assert.Nil(t, err)
+	assert.NoError(t, err)
+	assert.Equal(t, res, "76561197999073985")
+
+	res, err = SteamIdToCommId("[U:1:38808257:1]")
+	assert.NoError(t, err)
 	assert.Equal(t, res, "76561197999073985")
 
 	_, err = CommIdToSteamId("abcd")
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestLegacySteamIdToCommunityId(t *testing.T) {
 	res, err := SteamIdToCommId("STEAM_0:1:19404128")
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, res, "76561197999073985")
 
 	_, err = CommIdToSteamId("STEAM_1:1:19404128")
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 
 	_, err = CommIdToSteamId("")
-	assert.NotNil(t, err)
+	assert.Error(t, err)
+}
+
+func TestSteamIDsEqual(t *testing.T) {
+	eq := SteamIDsEqual("[U:1:38808257:1]", "[U:1:38808257]")
+	assert.True(t, eq)
 }
